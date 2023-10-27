de73d51b3aec598e44f488f752229680010cd2dd
lamduytr <lamduytr2182@gmail.com>
2023-10-27 09:09:02 +0700

    first commit

---
+++ app/src/main/java/com/example/mybookingcareapp/LoginActivity.java
@@ -0,0 +1,109 @@
+package com.example.mybookingcareapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.ktx.Firebase;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText editTextLoginEmail,editTextLoginPwd;
+    private ProgressBar progressBar;
+    private FirebaseAuth authProfile;
+    private static final String TAG ="LoginActivity";
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextLoginEmail = findViewById(R.id.editText_login_email);
+        editTextLoginPwd = findViewById(R.id.editText_login_pwd);
+        progressBar = findViewById(R.id.progressBar);
+        authProfile = FirebaseAuth.getInstance();
+
+        ImageView imageViewShowHidePwd = findViewById(R.id.imageView_show_hide_pwd);
+        imageViewShowHidePwd.setImageResource(R.drawable.img_1);
+        imageViewShowHidePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editTextLoginPwd.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())){
+                    editTextLoginPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.img_1);
+                } else {
+                    editTextLoginPwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.img);
+                }
+            }
+        });
+
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textEmail = editTextLoginEmail.getText().toString();
+                String textPwd = editTextLoginPwd.getText().toString();
+                if (TextUtils.isEmpty(textEmail)){
+                    Toast.makeText(LoginActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Email is required");
+                    editTextLoginEmail.requestFocus();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()){
+                    Toast.makeText(LoginActivity.this, "Please re-enter your email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Valid email is required");
+                    editTextLoginEmail.requestFocus();
+                } else if (TextUtils.isEmpty(textPwd)){
+                    Toast.makeText(LoginActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    editTextLoginPwd.setError("Password is required");
+                    editTextLoginPwd.requestFocus();
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(textEmail,textPwd);
+                }
+            }
+        });
+    }
+
+    private void loginUser(String email, String pwd) {
+        authProfile.signInWithEmailAndPassword(email,pwd).addOnCompleteListener(LoginActivity.this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "You are logged in now.", Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthInvalidUserException e) {
+                        editTextLoginEmail.setError("User does not exists is no longer valid.Please register again");
+                        editTextLoginEmail.requestFocus();
+                    } catch (FirebaseAuthInvalidCredentialsException e){
+                        editTextLoginEmail.setError("Invalid credentials.Kindly, check and re-enter");
+                        editTextLoginEmail.requestFocus();
+                    } catch (Exception e){
+                        Log.e(TAG, e.getMessage());
+                        Toast.makeText(LoginActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+}
